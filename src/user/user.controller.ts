baef54a43d12191e86c4aed31692e8415c8c284c
Abdulrahman Mohammed <57403427+Abdulrahman402@users.noreply.github.com>
2024-10-21 14:37:54 +0300

    added api swagger documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,25 +2,49 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Authenticate } from 'src/common/guard';
 import { Request } from 'express';
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { CurrentDto, WatchlistDto, RatingsDto } from './user.response.dto';
 
+@ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @UseGuards(Authenticate)
   @Get('current')
-  async current(@Req() req: Request) {
+  @ApiOkResponse({
+    description: 'Current user retrieved successfully.',
+    type: CurrentDto,
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access.' })
+  async current(@Req() req: Request): Promise<CurrentDto> {
     return this.userService.currentUser(req['user']._id);
   }
 
   @UseGuards(Authenticate)
   @Get('watchlist')
+  @ApiOkResponse({
+    description: 'User watchlist retrieved successfully.',
+    type: WatchlistDto,
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access.' })
   async getUserWithWatchlist(@Req() req: Request) {
     return this.userService.findUserWithWatchlist(req['user']._id);
   }
 
   @UseGuards(Authenticate)
   @Get('ratings')
+  @ApiOkResponse({
+    description: 'User rated movies retrieved successfully.',
+    type: [RatingsDto],
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access.' })
   async getUserWithRatedMovies(@Req() req: Request) {
     return this.userService.findUserWithRatedMovies(req['user']._id);
   }
